a16c6b12c54d1e24c2f0edf1120350eed1bb7368
move style to head <timmortal44@yahoo.com>
2023-04-28 23:58:53 +0100

    inlcude pricing

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -61,7 +61,7 @@ const Benefits = ({ setSelectedPage}: Props) => {
                     MORE THAN JUST A  GYM. 
                 </HText>
                 <p className="my-5 text-sm">
-                Meet expert trainers how all this mistaken idea of denouncing
+                Meet expert trainers, at an affordable pricing dea of denouncing
                 pleasure and praising pain was born and I will give you a complete
                 account of the system, and get access to cutting0edge equipment
                 </p>
